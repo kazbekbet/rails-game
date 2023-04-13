e95c2e2543ccb49fd2c88d43f5857629e4be45bb
kazbek.beteev <kazbek.beteev@accenture.com>
2023-04-13 14:08:55 +0300

    Небольшой рефактор: удалено всё лишнее.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,10 @@
-import './styles/app.scss';
 import { HtmlGame } from '@entities/html-game';
-
 
 function App() {
   return (
-    <div>
-
+    <>
       <HtmlGame />
-    </div>
+    </>
   );
 }
 
